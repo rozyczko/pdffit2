218ffcedf53099a45e68ec1432feec486b67549f
rozyczko <piotr.rozyczko@gmail.com>
2024-08-05 15:11:28 +0200

    structure is no longer 0.0.0

--- setup.py
+++ setup.py
@@ -224,7 +224,7 @@ setup_args = dict(
     data_files = data_files,
     install_requires = [
         'six',
-        'diffpy.structure==0.0.0',
+        'diffpy.structure',
     ],
     zip_safe = False,
 
